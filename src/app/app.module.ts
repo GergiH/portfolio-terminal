938458f281e8159da866013f4a12946d759e6d1c
GergiH <gergelyzsolt.hrv@gmail.com>
2022-05-24 08:17:18 +0200

    Add linter, components - headerbar, terminal view, regular view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderbarComponent } from './headerbar/headerbar.component';
+import { TerminalComponent } from './views/terminal/terminal.component';
+import { RegularComponent } from './views/regular/regular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderbarComponent,
+    TerminalComponent,
+    RegularComponent
   ],
   imports: [
     BrowserModule,
