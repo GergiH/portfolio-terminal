b3347c5576d79a9ebbf3d129204bf3e6c4ab3bc6
GergiH <gergelyzsolt.hrv@gmail.com>
2022-09-05 09:06:40 +0200

    Make site mobile friendly

--- src/components/TerminalView.vue
+++ src/components/TerminalView.vue
@@ -12,12 +12,15 @@
     </div>
 
     <div class="body" ref="terminalBody">
-      <ul class="terminal-ul">
-        <li v-for="(line, index) in lines" :key="line + index" v-html="line" />
+      <ul class="terminal-ul" v-if="windowWidth >= 800">
+        <li v-for="(line, index) in lines" :key="line.message + index" v-html="line.prompt + line.message" />
+      </ul>
+      <ul class="terminal-ul" v-if="windowWidth < 800">
+        <li v-for="(line, index) in lines" :key="line.message + index" v-html="line.smallPrompt + line.message" />
       </ul>
 
       <div class="command-line-container">
-        <div id="prompt" class="keep-spaces">{{ prompt }}</div>
+        <div id="prompt" class="keep-spaces">{{ windowWidth >= 800 ? prompt : '~ ' }}</div>
         <input
           id="command-line"
           v-model="command"
@@ -33,7 +36,7 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 
 const PROMPTPATH = '[visitor@gergih-portfolio] ';
 
@@ -53,7 +56,8 @@ export default {
     const lines = ref([]);
     const prompt = ref(`${convertDateToString(new Date())} ${PROMPTPATH}`);
     const terminalBody = ref(null);
-    let cmdIdx = ref(0);
+    const windowWidth = ref(window.innerWidth);
+    const cmdIdx = ref(0);
 
     const getPrompt = () => {
       return `${convertDateToString(new Date())} ${PROMPTPATH}`;
@@ -61,12 +65,45 @@ export default {
 
     // First few lines, also explaining the 'help' command
     const setInitialLines = () => {
-      lines.value.push('Interactive terminal, made by');
-      lines.value.push(String.raw`<span class="keep-spaces">_  _ ____ ____ _  _ ____ ___ _  _    ____ ____ ____ ____ ____ _    _   _ </span>`);
-      lines.value.push(String.raw`<span class="keep-spaces">|__| |  | |__/ |  | |__|  |  |__|    | __ |___ |__/ | __ |___ |     \_/  </span>`);
-      lines.value.push(String.raw`<span class="keep-spaces">|  | |__| |  \  \/  |  |  |  |  |    |__] |___ |  \ |__] |___ |___   |   </span>`);
-      lines.value.push('For available commands use \'<span class="help-command">help</span>\'');
-      lines.value.push('<br/>');
+      if (windowWidth.value >= 800) {
+        lines.value.push({
+          prompt: '',
+          smallPrompt: '',
+          message: 'Interactive terminal, made by'
+        });
+        lines.value.push({
+          prompt: '',
+          smallPrompt: '',
+          message: String.raw`<span class="keep-spaces">_  _ ____ ____ _  _ ____ ___ _  _    ____ ____ ____ ____ ____ _    _   _ </span>`
+        });
+        lines.value.push({
+          prompt: '',
+          smallPrompt: '',
+          message: String.raw`<span class="keep-spaces">|__| |  | |__/ |  | |__|  |  |__|    | __ |___ |__/ | __ |___ |     \_/  </span>`
+        });
+        lines.value.push({
+          prompt: '',
+          smallPrompt: '',
+          message: String.raw`<span class="keep-spaces">|  | |__| |  \  \/  |  |  |  |  |    |__] |___ |  \ |__] |___ |___   |   </span>`
+        });
+      } else {
+        lines.value.push({
+          prompt: '',
+          smallPrompt: '',
+          message: 'Interactive terminal, made by: Horváth Gergely Zsolt'
+        });
+      }
+
+      lines.value.push({
+        prompt: '',
+        smallPrompt: '',
+        message: 'For available commands use \'<span class="help-command">help</span>\''
+      });
+      lines.value.push({
+        prompt: '',
+        smallPrompt: '',
+        message: '<br/>'
+      });
     };
 
     const handleCommandHistory = (isDownArrow) => {
@@ -87,12 +124,9 @@ export default {
     const handleCommand = async () => {
       await setContentAsync();
       prompt.value = getPrompt();
-
       // Handle the list of commands in the App component, so we always have the history
       context.emit('setInputtedCommands', command.value);
-
       command.value = '';
-
       terminalBody.value.scrollTop = terminalBody.value.scrollHeight;
     };
 
@@ -101,41 +135,74 @@ export default {
     };
 
     const setContentAsync = async () => {
-        lines.value.push(`<span class="keep-spaces">${getPrompt()}</span><span>${command.value}</span>`);
+      lines.value.push({
+        prompt: `<span class="keep-spaces">${getPrompt()}</span>`,
+        smallPrompt: '~ ',
+        message: `<span>${command.value}</span>`
+      });
 
-        switch (command.value) {
-          case props.commandNames.about:
-            lines.value.push(props.commands.about);
-            break;
-          case props.commandNames.career:
-            lines.value.push(props.commands.career);
-            break;
-          case props.commandNames.clear:
-            lines.value = [];
-            // setInitialLines();
-            break;
-          case props.commandNames.help:
-            for (const description of props.commands.help) {
-              lines.value.push(`<li><span class="help-command">${description[0]}</span> - ${description[1]}</li>`);
-            }
-            break;
-          case props.commandNames.links:
-            for (const link of props.commands.links) {
-              lines.value.push(`<li>${link[0]} - <a href="${link[1]}" target="_blank">${link[1]}</a></li>`);
-            }
-            break;
-          case '':
-          case undefined:
-            break;
-          default:
-            lines.value.push(handleInvalidCommand(command));
-            break;
-        }
+      switch (command.value) {
+        case props.commandNames.about:
+          lines.value.push({
+            prompt: '',
+            smallPrompt: '',
+            message: props.commands.about
+          });
+          break;
+        case props.commandNames.career:
+          lines.value.push({
+            prompt: '',
+            smallPrompt: '',
+            message: props.commands.career
+          });
+          break;
+        case props.commandNames.clear:
+          lines.value = [];
+          // setInitialLines();
+          break;
+        case props.commandNames.help:
+          for (const description of props.commands.help) {
+            lines.value.push({
+              prompt: '',
+              smallPrompt: '',
+              message: `<li><span class="help-command">${description[0]}</span> - ${description[1]}</li>`
+            });
+          }
+          break;
+        case props.commandNames.links:
+          for (const link of props.commands.links) {
+            lines.value.push({
+              prompt: '',
+              smallPrompt: '',
+              message: `<li>${link[0]} - <a href="${link[1]}" target="_blank">${link[1]}</a></li>`
+            });
+          }
+          break;
+        case '':
+        case undefined:
+          break;
+        default:
+          lines.value.push({
+            prompt: '',
+            smallPrompt: '',
+            message: handleInvalidCommand(command)
+          });
+          break;
+      }
+    };
+
+    const resizeListener = () => {
+      windowWidth.value = window.innerWidth;
     };
 
     onMounted(() => {
       setInitialLines();
-    })
+      window.addEventListener('resize', resizeListener);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener('resize', resizeListener);
+    });
 
     return {
       command,
@@ -144,7 +211,8 @@ export default {
       handleCommandHistory,
       lines,
       prompt,
-      terminalBody
+      terminalBody,
+      windowWidth
     };
   }
 }
